5cdd2328a15a02f12553d55561e5849cd94dc8f8
Shameek Hargrave <shargraveseeds@gmail.com>
2022-10-24 19:31:10 -0400

    success queires

--- source_ingredients.py
+++ source_ingredients.py
@@ -7,7 +7,8 @@
 # food item in the list.
 #----------------------------------------------------------------#
 import argparse
-
+import requests
+import json
 #----------------------------------------------------------------#
 def set_cmd_args():
     parser = argparse.ArgumentParser(description=
@@ -18,14 +19,34 @@ def set_cmd_args():
     return parser
 #----------------------------------------------------------------#
 def main():
-    parser = set_cmd_args()
-    args = parser.parse_args()
-    food_list = args.food_list
+    # parser = set_cmd_args()
+    # args = parser.parse_args()
+    # food_list = args.food_list
     # structure 
-    # [ { name: "bacon", category: "Meat" }, 
-    # { name: "pork chop", category: "Meat" }] 
+    food_list = [ 
+        { "name": "bacon", "category": "Meat" },
+        { "name": "pork chop", "category": "Meat" }
+    ] 
     # connect to API
 
+    params = dict(
+        # dataType = [],
+        pageSize = 1,
+    )
+    url = 'https://api.nal.usda.gov/fdc/v1/foods/search?query='
+    # url += '&api_key=z47Rw2u6Lx680CxpaSEes2tvNUk6fAw3AUtfEHwG'
+    for food in food_list:
+        full_query = url + food["name"] + '&'
+        full_query += 'api_key=z47Rw2u6Lx680CxpaSEes2tvNUk6fAw3AUtfEHwG'
+        resp = requests.get(url=full_query, params=params)
+        data = resp.json()
+        data = data["foods"][0]["foodNutrients"]
+        # protein = data.get("nutrie")
+        # protein id: 1003
+        # carbs id: 1005
+        # fats id: 1004,
+        # calories id: 1008
+        print(data.keys())
 
     return None
 
